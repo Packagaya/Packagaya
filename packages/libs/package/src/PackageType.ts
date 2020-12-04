9111be42be06a2c33bdd8b292bdd7d6366b23d39
Yannick Fricke <yannickfricke@googlemail.com>
2020-12-04 19:12:10 +0100

    feat(package): added the PackageType enum
    The PackageType enum is used to define the type of a package (application / library)

---
+++ packages/libs/package/src/PackageType.ts
@@ -0,0 +1,12 @@
+/**
+ * Defines the package type
+ *
+ * The package type can be Application or Library
+ *
+ * @export
+ * @enum {number}
+ */
+export enum PackageType {
+    Application,
+    Library,
+}
