ced347d2074e8e814f15e79473ab6dcb585d03e0
Yannick Fricke <yannickfricke@googlemail.com>
2020-12-05 00:10:35 +0100

    refactor(cli): the Application should load the configuration file
    When an error occurs the process will be terminated with the exit code "1"

--- packages/apps/cli/src/Application.ts
+++ packages/apps/cli/src/Application.ts
@@ -1,9 +1,20 @@
 import { inject, injectable } from 'inversify';
 import { Logger } from 'tslog';
+import { ConfigManager } from '@packagaya/config/dist/ConfigManager';
 
 @injectable()
 export class Application {
-    constructor(@inject(Logger) private logger: Logger) {}
+    constructor(
+        @inject(Logger) private logger: Logger,
+        @inject(ConfigManager) private configManager: ConfigManager,
+    ) {}
 
-    public run() {}
+    public run() {
+        try {
+            const projectConfiguration = this.configManager.readConfiguration();
+        } catch (error) {
+            this.logger.error(error);
+            process.exit(1);
+        }
+    }
 }
