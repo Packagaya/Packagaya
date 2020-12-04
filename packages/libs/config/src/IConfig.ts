849c6f0c2142aa19bd75851ace18e84007eeb536
Yannick Fricke <yannickfricke@googlemail.com>
2020-12-04 19:23:19 +0100

    chore(packages): initialized the config package
    Added the package.json, tsconfig.json and the definition of the IConfig interface

---
+++ packages/libs/config/src/IConfig.ts
@@ -0,0 +1,39 @@
+/**
+ * Defines the basic configuration of Packagaya
+ *
+ * @export
+ * @interface IConfig
+ */
+export interface IConfig {
+    /**
+     * The paths to all application packages
+     *
+     * @type {string[]}
+     * @memberof IConfig
+     */
+    apps: string[];
+
+    /**
+     * The paths to all library packages
+     *
+     * @type {string[]}
+     * @memberof IConfig
+     */
+    libs: string[];
+
+    /**
+     * The adapters which should be used
+     *
+     * @type {string[]}
+     * @memberof IConfig
+     */
+    adapters: string[];
+
+    /**
+     * The activated features of the adapters
+     *
+     * @type {string[]}
+     * @memberof IConfig
+     */
+    features: string[];
+}
