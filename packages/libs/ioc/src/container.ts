54d3b84cf4a82d98cdcaf027a01cc99d61b4ea9d
Yannick Fricke <yannickfricke@googlemail.com>
2020-12-05 00:07:18 +0100

    refactor(ioc): the container should load the validator module

--- packages/libs/ioc/src/container.ts
+++ packages/libs/ioc/src/container.ts
@@ -1,12 +1,14 @@
 import { Container } from 'inversify';
 import { ConfigModule } from './modules/ConfigModule';
 import { LoggerModule } from './modules/LoggerModule';
+import { ValidatorModule } from './modules/ValidatorModule';
 
 export const getContainer = () => {
     const container = new Container();
 
     container.load(new LoggerModule());
     container.load(new ConfigModule());
+    container.load(new ValidatorModule());
 
     return container;
 };
