a5b9f6a2fbb0abb62ece845f2b6c58f57ba8ad60
Yannick Fricke <yannickfricke@googlemail.com>
2020-12-05 00:04:32 +0100

    feat(ioc): added the validator module
    The validator module binds "ajv" to the container. ajv itself is configured to include the schema
    definitions for the configuration file JSON schema.

---
+++ packages/libs/ioc/src/modules/ValidatorModule.ts
@@ -0,0 +1,17 @@
+import { ContainerModule } from 'inversify';
+import Ajv from 'ajv';
+import ConfigSchema from '../../../config/ConfigSchema.json';
+
+export class ValidatorModule extends ContainerModule {
+    constructor() {
+        super((bind) => {
+            bind(Ajv).toDynamicValue(() => {
+                const ajv = new Ajv();
+
+                ajv.addSchema(ConfigSchema, 'config');
+
+                return ajv;
+            });
+        });
+    }
+}
