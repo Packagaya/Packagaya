be3e88d7a73f3166ced4d9f71eece93522f6ea7e
Yannick Fricke <yannickfricke@googlemail.com>
2020-12-06 09:01:52 +0100

    test(config): Added a test for the ConfigManager
    Jest: Set up to use "setup files" which load reflect-metadata and stuff
    Jest: Added "setup-jest.ts" for importing required dependencies while testing

---
+++ packages/libs/config/src/__tests__/ConfigManager.test.ts
@@ -0,0 +1,7 @@
+import { ConfigManager } from '../ConfigManager';
+
+describe('Config Manager', () => {
+    it('should be defined', () => {
+        expect(ConfigManager).toBeDefined();
+    });
+});
