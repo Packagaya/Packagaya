aea3745e9a8fc7c86f75580861a2a31b51ba3fa2
Yannick Fricke <yannickfricke@googlemail.com>
2020-12-09 06:08:39 +0100

    refactor(config): the "ConfigModule" should not bind the ConfigManager in singleton mode
    The ConfigManager does not have any state which should be saved between instantiation calls so
    singleton is not the thing we want.

--- packages/libs/ioc/src/modules/ConfigModule.ts
+++ packages/libs/ioc/src/modules/ConfigModule.ts
@@ -1,10 +1,21 @@
 import { ContainerModule } from 'inversify';
 import { ConfigManager } from '@packagaya/config/dist/ConfigManager';
 
+/**
+ * The ConfigModule binds all the stuff from the config package to the container
+ *
+ * @export
+ * @class ConfigModule
+ * @extends {ContainerModule}
+ */
 export class ConfigModule extends ContainerModule {
+    /**
+     * Creates an instance of ConfigModule.
+     * @memberof ConfigModule
+     */
     constructor() {
         super((bind) => {
-            bind(ConfigManager).toSelf().inSingletonScope();
+            bind(ConfigManager).toSelf();
         });
     }
 }
