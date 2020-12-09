abdaed24aa60907c21a90cbcf6b4947e3b005e48
Yannick Fricke <yannickfricke@googlemail.com>
2020-12-09 06:05:13 +0100

    feat(ioc): load the "LocalFileSystemModule"
    The "LocalFileSystemModule" should be loaded by the dependency injection container

--- packages/libs/ioc/src/container.ts
+++ packages/libs/ioc/src/container.ts
@@ -2,6 +2,7 @@ import { Container } from 'inversify';
 import { ConfigModule } from './modules/ConfigModule';
 import { LoggerModule } from './modules/LoggerModule';
 import { ValidatorModule } from '@packagaya/config/dist/ioc/ValidatorModule';
+import { LocalFileSystemModule } from './modules/LocalFileSystemModule';
 
 export const getContainer = () => {
     const container = new Container();
@@ -9,6 +10,7 @@ export const getContainer = () => {
     container.load(new LoggerModule());
     container.load(new ConfigModule());
     container.load(new ValidatorModule());
+    container.load(new LocalFileSystemModule());
 
     return container;
 };
