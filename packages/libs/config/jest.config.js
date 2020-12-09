936531c0c76fda699c95cc6b73c63dd7fbb2a33c
Yannick Fricke <yannickfricke@googlemail.com>
2020-12-09 05:50:22 +0100

    chore(config): refactored the jest config file
    Added the "testMatch" patterns and beautified the contents

--- packages/libs/config/jest.config.js
+++ packages/libs/config/jest.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testPathIgnorePatterns: ["/node_modules/", "/dist/"],
-  setupFiles: [
-    "./src/setup-jest.ts",
-  ]
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+    testMatch: [
+        '**/*.test.ts',
+    ],
+    setupFiles: ['./src/setup-jest.ts'],
 };
