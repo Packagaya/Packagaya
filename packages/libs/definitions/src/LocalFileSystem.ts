2e4d0af4ab97eeb2400c61ed0af64e226e9c18ab
Yannick Fricke <yannickfricke@googlemail.com>
2020-12-09 06:03:32 +0100

    feat(definitions): added the "checkIfDirectoryExists" function

--- packages/libs/definitions/src/LocalFileSystem.ts
+++ packages/libs/definitions/src/LocalFileSystem.ts
@@ -1,5 +1,5 @@
 import { injectable } from 'inversify';
-import { readFileSync, writeFileSync, existsSync } from 'fs';
+import { readFileSync, writeFileSync, existsSync, statSync } from 'fs';
 import { resolve } from 'path';
 
 /**
@@ -12,18 +12,24 @@ export class LocalFileSystem {
      * Reads the contents of the file at the specified path
      *
      * @returns {string} The contents of the read file
+     * @public
      */
     public readFile = (filePath: string): string =>
         readFileSync(filePath).toString('utf-8');
 
     /**
      * Writes a file to the disk with the given file contents
+     *
+     * @public
      */
     public writeFile = (filePath: string, fileContents: string) =>
         writeFileSync(filePath, fileContents);
 
     /**
+     * Checks if the given path is a file
+     *
      * @returns {boolean} True when the file exists. Otherwise false is returned.
+     * @public
      */
     public checkIfFileExists = (filePath: string): boolean =>
         existsSync(filePath);
@@ -31,7 +37,18 @@ export class LocalFileSystem {
     /**
      * @returns {string} Returns the resolved path from the given segments.
      *                   This function does not checks if the path is existent or not.
+     * @public
      */
     public resolve = (...pathSegments: string[]): string =>
         resolve(...pathSegments);
+
+    /**
+     * Checks if the given path is a directory
+     *
+     * @returns {boolean} True when the given path is a directory. Otherwise false is returned.
+     * @public
+     */
+    public checkIfDirectoryExists(path: string): boolean {
+        return statSync(path).isDirectory();
+    }
 }
