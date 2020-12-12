ddd9068271f40d85979c4dcc0a994c7dd2610ebd
Yannick Fricke <yannickfricke@googlemail.com>
2020-12-12 20:41:16 +0100

    refactor(ioc): load the adapter module
    Also add the typescript path definition for the adapter package

--- packages/libs/ioc/src/container.ts
+++ packages/libs/ioc/src/container.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from './modules/ConfigModule';
 import { LoggerModule } from './modules/LoggerModule';
 import { ValidatorModule } from '@packagaya/config/dist/ioc/ValidatorModule';
 import { LocalFileSystemModule } from './modules/LocalFileSystemModule';
+import { AdapterModule } from './modules/AdapterModule';
 
 export const getContainer = () => {
     const container = new Container();
@@ -11,6 +12,7 @@ export const getContainer = () => {
     container.load(new ConfigModule());
     container.load(new ValidatorModule());
     container.load(new LocalFileSystemModule());
+    container.load(new AdapterModule());
 
     return container;
 };
