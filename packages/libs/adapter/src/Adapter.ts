d00ba35d8bff27e91180e0cdd4a977da140195b8
Yannick Fricke <yannickfricke@googlemail.com>
2020-12-14 13:07:49 +0100

    refactor(adapter): refactored constructor parameters of the Adapter
    Marked the constructor parameters of the Adapter class as unmanaged for inversify. Also updated the
    import to the feature flag.

--- packages/libs/adapter/src/Adapter.ts
+++ packages/libs/adapter/src/Adapter.ts
@@ -1,11 +1,29 @@
-import { injectable } from 'inversify';
-import { FeatureFlag } from '@packagaya/definitions/dist/FeatureFlag';
+import { injectable, unmanaged } from 'inversify';
+import { FeatureFlag } from './FeatureFlag';
 
+/**
+ * Defines a basic adapter which can be extended by other packages
+ * An adapter is like a group for custom commands, feature flags
+ * and so many more!
+ *
+ * @export
+ * @abstract
+ * @class Adapter
+ */
 @injectable()
 export abstract class Adapter {
+    /**
+     * Creates an instance of Adapter.
+     * @param {string} name The name of the adapter
+     * @param {FeatureFlag[]} [featureFlags=[]] The feature flags of the adapter
+     * @memberof Adapter
+     */
     protected constructor(
+        @unmanaged()
         public readonly name: string,
+        @unmanaged()
         public readonly featureFlags: FeatureFlag[] = [],
-    ) {
-    }
+    ) {}
+
+    public abstract init(): void;
 }
