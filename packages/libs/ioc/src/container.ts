32ffd8b5a55a6c875b409fc292f109edfcdf472a
Yannick Fricke <yannickfricke@googlemail.com>
2020-12-14 13:24:26 +0100

    refactor(ioc): load the command module
    The "getContainer" method should load the command container module. Also refactored the load call to
    load a list of container modules instead of having multiple single calls to the method.
    Also added the command package as path to the tsconfig.json file.

--- packages/libs/ioc/src/container.ts
+++ packages/libs/ioc/src/container.ts
@@ -4,15 +4,38 @@ import { LoggerModule } from './modules/LoggerModule';
 import { ValidatorModule } from '@packagaya/config/dist/ioc/ValidatorModule';
 import { LocalFileSystemModule } from './modules/LocalFileSystemModule';
 import { AdapterModule } from './modules/AdapterModule';
+import { CommandModule } from './modules/CommandModule';
 
-export const getContainer = () => {
+/**
+ * Loads the container with all known container modules
+ *
+ * @returns The prepared container
+ */
+export const getContainer = (): Container => {
+    // Create a new instance of the dependency injection container
     const container = new Container();
 
-    container.load(new LoggerModule());
-    container.load(new ConfigModule());
-    container.load(new ValidatorModule());
-    container.load(new LocalFileSystemModule());
-    container.load(new AdapterModule());
+    // Load all required container modules
+    container.load(
+        // Binds the tslog logger
+        new LoggerModule(),
 
+        // Binds the configuration manager and other config related stuff
+        new ConfigModule(),
+
+        // Bind the validator module for validating json schemas
+        new ValidatorModule(),
+
+        // Bind the local filesystem
+        new LocalFileSystemModule(),
+
+        // Bind all offical adapters
+        new AdapterModule(),
+
+        // Bind all system commands
+        new CommandModule(),
+    );
+
+    // Return the prepared container
     return container;
 };
