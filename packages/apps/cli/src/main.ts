761a996a0bc13fc944b9b10ed04a5ad7a8c392bd
Yannick Fricke <yannickfricke@googlemail.com>
2020-12-14 13:04:46 +0100

    refactor(cli): refactored the main entrypoint
    Bound the inversify container to itself by "Container.name". Also the run method of the application
    will be awaited.

--- packages/apps/cli/src/main.ts
+++ packages/apps/cli/src/main.ts
@@ -2,12 +2,20 @@ import 'reflect-metadata';
 import { getContainer } from '@packagaya/ioc/dist/container';
 import { ApplicationModule } from './ioc/ApplicationModule';
 import { Application } from './Application';
+import { Container } from 'inversify';
 
 (async () => {
     const container = getContainer();
 
+    // Bind the container to itself
+    container.bind(Container.name).toConstantValue(container);
+
+    // Bind the specific application module
     container.load(new ApplicationModule());
 
+    // Get the application from the dependency injection container
     const application = container.get(Application);
-    application.run();
+
+    // Run and await the application
+    await application.run();
 })();
