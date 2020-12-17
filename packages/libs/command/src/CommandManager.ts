ca21c6603f3b59d3fa0a50a34833fdc954b6e307
Yannick Fricke <yannickfricke@googlemail.com>
2020-12-17 01:30:59 +0100

    style(commands): adjust the style for the CommandManager
    Sorted imports and added more documentation to the file

--- packages/libs/command/src/CommandManager.ts
+++ packages/libs/command/src/CommandManager.ts
@@ -1,18 +1,34 @@
-import { Command } from './Command';
-import { injectable, multiInject } from 'inversify';
 import { IConfig } from '@packagaya/config/dist/IConfig';
+import { injectable, multiInject } from 'inversify';
+
+import { Command } from './Command';
 
 @injectable()
 export class CommandManager {
-    constructor(
-        @multiInject(Command) private readonly commands: Command[],
-    ) {
-    }
+    /**
+     * Creates an instance of CommandManager.
+     * @param {Command[]} commands The commands which are managed by the command manager
+     * @memberof CommandManager
+     */
+    constructor(@multiInject(Command) private readonly commands: Command[]) {}
 
+    /**
+     * Returns all registered commands
+     *
+     * @return {*}  {Command[]} All registered commands
+     * @memberof CommandManager
+     */
     getCommands(): Command[] {
         return this.commands;
     }
 
+    /**
+     * Registers a new command
+     *
+     * @param {Command} command
+     * @throws {Error} When the command is already registered
+     * @memberof CommandManager
+     */
     registerCommand(command: Command) {
         if (this.isCommandRegistered(command)) {
             throw `The command ${command.name} is already registered`;
@@ -21,7 +37,20 @@ export class CommandManager {
         this.commands.push(command);
     }
 
-    async executeCommand(commandName: string, projectSpecification: IConfig, commandArguments: string[] = []) {
+    /**
+     * Executes the command by the given name
+     *
+     * @param {string} commandName The name of the command which should be executed
+     * @param {IConfig} projectSpecification The project configuration
+     * @param {string[]} [commandArguments=[]] Additional command arguments for the command
+     * @memberof CommandManager
+     */
+    async executeCommand(
+        commandName: string,
+        projectSpecification: IConfig,
+        commandArguments: string[] = [],
+    ) {
+        // Check if the command by the given name exists
         const foundCommand = this.commands.find(
             (registeredCommand: Command) => {
                 return (
@@ -31,52 +60,87 @@ export class CommandManager {
             },
         );
 
+        // Check if a command was found
         if (foundCommand === undefined) {
+            // Throw an error when no command matched the command name
             throw `No command with the name or alias ${commandName} found`;
         }
 
+        // Find the correct sub command by the given command arguments
         const subCommand = this.findSubCommand(foundCommand, commandArguments);
 
+        // Define the variable for the command
+        // which should be executed
         let commandToExecute: Command;
 
+        // Check if a sub command was found
         if (subCommand === undefined) {
+            // A sub command was not found, so we assign the existing command
             commandToExecute = foundCommand;
         } else {
+            // A sub command was found, so we assign it instead of the existing command
             commandToExecute = subCommand;
         }
 
+        // Execute the found command with the given project configuration and additional command arguments
         await commandToExecute.execute(projectSpecification, commandArguments);
     }
 
+    /**
+     * Finds the correct sub command from all registered commands
+     *
+     * @private
+     * @param {Command} currentCommand The current command which was determined by the CLI
+     * @param {string[]} commandArguments The command arguments
+     * @return {(Command | undefined)} The found command or undefined
+     * @memberof CommandManager
+     */
     private findSubCommand(
         currentCommand: Command,
         commandArguments: string[],
     ): Command | undefined {
+        // Extract the sub command name
         const subCommandName = commandArguments[0];
 
-        const foundSubCommand = currentCommand.subCommands.find(subCommand => {
-            return (
-                subCommand.name === subCommandName ||
-                subCommand.aliases.includes(subCommandName)
-            );
-        });
+        // Find the sub command by its name in the sub commands from the current command
+        const foundSubCommand = currentCommand.subCommands.find(
+            (subCommand) => {
+                return (
+                    subCommand.name === subCommandName ||
+                    subCommand.aliases.includes(subCommandName)
+                );
+            },
+        );
 
+        // Check if we found a sub command
         if (foundSubCommand === undefined) {
+            // A sub command was not found so we return undefined
             return undefined;
         }
 
+        // Take one command argument since we want to find other sub-sub-sub-...-commands
         commandArguments.shift();
 
+        // Search for more sub commands!
         const subSubCommand = this.findSubCommand(
             foundSubCommand,
             commandArguments.slice(1),
         );
 
+        // Return the found sub command or undefined when none was found
         return subSubCommand === undefined ? foundSubCommand : subSubCommand;
     }
 
+    /**
+     * Checks if the given command is already registered
+     *
+     * @private
+     * @param {Command} command The command that should be checked
+     * @return {boolean} True when the command is already registered
+     * @memberof CommandManager
+     */
     private isCommandRegistered(command: Command): boolean {
-        return this.commands.some(registeredCommand => {
+        return this.commands.some((registeredCommand) => {
             return (
                 registeredCommand.name === command.name ||
                 registeredCommand.aliases.includes(command.name)
