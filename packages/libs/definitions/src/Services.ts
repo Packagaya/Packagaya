2082eb2ab7864e1e7485821590cfe277f430ec4f
Yannick Fricke <yannickfricke@googlemail.com>
2020-12-17 01:21:27 +0100

    docs(definitions): added more docs to the Services constants

--- packages/libs/definitions/src/Services.ts
+++ packages/libs/definitions/src/Services.ts
@@ -1,5 +1,8 @@
+// Defines the IoC service names
 export const Services = {
+    // The schema object
     Schema: {
+        // The symbol for the schema configuration
         Config: Symbol.for('Schema.Config'),
     },
 };
