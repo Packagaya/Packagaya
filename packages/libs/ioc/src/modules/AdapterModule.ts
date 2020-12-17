cded2b6c0dbaa23d484cf83281bcf6bc5bad33c4
Yannick Fricke <yannickfricke@googlemail.com>
2020-12-17 01:28:34 +0100

    refactor(ioc): bound different classes in the adapter module
    Bound the AdapterLoader, ModuleLoader and the ContainerBindingsLoader to the container

--- packages/libs/ioc/src/modules/AdapterModule.ts
+++ packages/libs/ioc/src/modules/AdapterModule.ts
@@ -1,10 +1,26 @@
 import { ContainerModule } from 'inversify';
 import { AdapterLoader } from '@packagaya/adapter/dist/AdapterLoader';
+import { ContainerBindingsLoader } from '@packagaya/adapter/dist/ContainerBindingsLoader';
+import { ModuleLoader } from '@packagaya/adapter/dist/ModuleLoader';
 
+/**
+ * Defines an IoC container module for the adapter package
+ *
+ * @export
+ * @class AdapterModule
+ * @extends {ContainerModule}
+ */
 export class AdapterModule extends ContainerModule {
     constructor() {
         super((bind) => {
+            // Bind the adapter loader to the container
             bind(AdapterLoader).toSelf();
+
+            // Bind the module loader to the container
+            bind(ModuleLoader).toSelf();
+
+            // Bind the container bindings loader to the container
+            bind(ContainerBindingsLoader).toSelf();
         });
     }
 }
