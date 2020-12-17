722387c2273648fd4f3363f708744222648c45c2
Yannick Fricke <yannickfricke@googlemail.com>
2020-12-17 01:33:27 +0100

    refactor(ioc): refactored the LoggerModule
    Moved the logger to the system namespace because we only want one instance of that logger every time

--- packages/libs/ioc/src/modules/LoggerModule.ts
+++ packages/libs/ioc/src/modules/LoggerModule.ts
@@ -1,16 +1,32 @@
 import { ContainerModule } from 'inversify';
 import { Logger } from 'tslog';
 
+// The logger is created in the
+// global namespace because we only
+// want a single instance across the process
+const logger = new Logger({
+    minLevel: (process.env.LOG_LEVEL as 'info') ?? 'silly',
+});
+
+/**
+ * Defines an IoC container module for the logger package
+ *
+ * @export
+ * @class LoggerModule
+ * @extends {ContainerModule}
+ */
 export class LoggerModule extends ContainerModule {
+    /**
+     * Creates an instance of LoggerModule.
+     * @memberof LoggerModule
+     * @see logger The logger which will be used to log
+     *             messages
+     */
     constructor() {
         super((bind) => {
-            bind(Logger)
-                .toDynamicValue(() => {
-                    return new Logger({
-                        minLevel: (process.env.LOG_LEVEL as 'info') ?? 'silly',
-                    });
-                })
-                .inSingletonScope();
+            // Bind the logger class from tslog to
+            // the global defined logger
+            bind(Logger).toConstantValue(logger);
         });
     }
 }
