9d6a86df417ea2845d1e9e2bbf96ecfe2de207a8
Yannick Fricke <yannickfricke@googlemail.com>
2020-12-17 01:32:27 +0100

    docs(ioc): added docs for the CommandModule

--- packages/libs/ioc/src/modules/CommandModule.ts
+++ packages/libs/ioc/src/modules/CommandModule.ts
@@ -3,11 +3,20 @@ import { CommandManager } from '@packagaya/command/dist/CommandManager';
 import { Command } from '@packagaya/command/dist/Command';
 import { SyncCommand } from '@packagaya/command/dist/commands/SyncCommand';
 
+/**
+ * Defines an IoC container module for the command package
+ *
+ * @export
+ * @class CommandModule
+ * @extends {ContainerModule}
+ */
 export class CommandModule extends ContainerModule {
     constructor() {
         super((bind) => {
+            // Bind the command manager to the container
             bind(CommandManager).toSelf().inSingletonScope();
 
+            // Bind the "sync" command to the container
             bind(Command).to(SyncCommand);
         });
     }
