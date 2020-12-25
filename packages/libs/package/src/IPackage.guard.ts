67cb9896e7a9560ef5e00e391ad6ee281d6abd2e
Yannick Fricke <yannickfricke@googlemail.com>
2020-12-25 21:03:44 +0100

    refactor(ipackage): updated the guard
    Updated the guard for the "IPackage" interface

--- packages/libs/package/src/IPackage.guard.ts
+++ packages/libs/package/src/IPackage.guard.ts
@@ -5,29 +5,9 @@
 import { IDependency, IPackage } from './IPackage';
 import { PackageType } from './PackageType';
 
-export function isIDependency(
-    obj: any,
-    _argumentName?: string,
-): obj is IDependency {
+export function isRecord(obj: any, _argumentName?: string): obj is IDependency {
     return (
-        (((obj !== null && typeof obj === 'object') ||
-            typeof obj === 'function') &&
-            typeof obj.name === 'string' &&
-            typeof obj.version === 'string' &&
-            Array.isArray(obj.dependencies) &&
-            obj.dependencies.every((e: any) => isIDependency(e) as boolean) &&
-            Array.isArray(obj.devDependencies) &&
-            obj.devDependencies.every(
-                (e: any) => isIDependency(e) as boolean,
-            )) ||
-        (((obj !== null && typeof obj === 'object') ||
-            typeof obj === 'function') &&
-            typeof obj.name === 'string' &&
-            typeof obj.version === 'string' &&
-            Array.isArray(obj.dependencies) &&
-            obj.dependencies.every((e: any) => isIDependency(e) as boolean) &&
-            Array.isArray(obj.devDependencies) &&
-            obj.devDependencies.every((e: any) => isIDependency(e) as boolean))
+        (obj !== null && typeof obj === 'object') || typeof obj === 'function'
     );
 }
 
@@ -40,9 +20,8 @@ export function isIPackage(obj: any, _argumentName?: string): obj is IPackage {
         typeof obj.version === 'string' &&
         (obj.packageType === PackageType.Application ||
             obj.packageType === PackageType.Library) &&
-        Array.isArray(obj.dependencies) &&
-        obj.dependencies.every((e: any) => isIDependency(e) as boolean) &&
-        Array.isArray(obj.devDependencies) &&
-        obj.devDependencies.every((e: any) => isIDependency(e) as boolean)
+        typeof obj.path === 'string' &&
+        (isRecord(obj.dependencies) as boolean) &&
+        (isRecord(obj.devDependencies) as boolean)
     );
 }
