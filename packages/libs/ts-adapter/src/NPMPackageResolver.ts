bf8b3c7724606edfe070a14030fe062d9515d009
Yannick Fricke <yannickfricke@googlemail.com>
2021-01-02 11:29:35 +0100

    feat(NPMPackageResolver): added source directories support
    The NPMPackageResolver now resolves source code directories based on the "tsconfig.json" file.
    It checks the following keys for directories and resolves the glob automatically:
    - compilerOptions.rootDir
    - compilerOptions.rootDirs
    - files
    - include
    - exclude

--- packages/libs/ts-adapter/src/NPMPackageResolver.ts
+++ packages/libs/ts-adapter/src/NPMPackageResolver.ts
@@ -2,6 +2,7 @@ import { LocalFileSystem } from '@packagaya/definitions/dist/LocalFileSystem';
 import { IPackage } from '@packagaya/package/dist/IPackage';
 import { PackageResolver } from '@packagaya/package/dist/PackageResolver';
 import { PackageType } from '@packagaya/package/src/PackageType';
+import { hasMagic, sync } from 'glob';
 import { inject, injectable } from 'inversify';
 import { Logger } from 'tslog';
 
@@ -60,6 +61,10 @@ export class NPMPackageResolver extends PackageResolver {
             'package.json',
         );
 
+        this.logger.silly(
+            `Checking the following configuration file path: ${configurationFilePath}`,
+        );
+
         if (!this.fileSystem.checkIfFileExists(configurationFilePath)) {
             throw new Error(
                 `The file ${configurationFilePath} does not exists`,
@@ -67,6 +72,11 @@ export class NPMPackageResolver extends PackageResolver {
         }
 
         const fileContents = this.fileSystem.readFile(configurationFilePath);
+
+        this.logger.silly(
+            `Reading configuration file: ${configurationFilePath}`,
+        );
+
         return JSON.parse(fileContents);
     }
 
@@ -85,14 +95,140 @@ export class NPMPackageResolver extends PackageResolver {
     ): IPackage {
         const packageConfiguration = this.getPackageConfiguration(packagePath);
 
+        this.logger.silly(
+            'Resolved the following package configuration',
+            packageConfiguration,
+        );
+
         return {
-            id: packageConfiguration.name,
             name: packageConfiguration.name,
             version: packageConfiguration.version,
             packageType,
             path: packagePath,
+            sourceDirectories: this.getTypeScriptSourceDirectories(packagePath),
             dependencies: packageConfiguration.dependencies ?? {},
             devDependencies: packageConfiguration.devDependencies ?? {},
         };
+    }
+
+    /**
+     * Returns an array of strings which points to directories which contain source files
+     *
+     * @private
+     * @param {string} path The base path to the package
+     * @return {string[]} The resolved directories
+     * @memberof NPMPackageResolver
+     */
+    private getTypeScriptSourceDirectories(path: string): string[] {
+        // Resolve the path to the tsconfig.json file
+        const typescriptConfigurationFile = this.fileSystem.resolve(
+            path,
+            'tsconfig.json',
+        );
+
+        // The function result
+        let result: string[] = [];
+
+        this.logger.silly(
+            `Checking if the TypeScript configuration file exists: ${typescriptConfigurationFile}`,
+        );
+
+        // Check if the TypeScript configuration file exists
+        if (!this.fileSystem.checkIfFileExists(typescriptConfigurationFile)) {
+            // The configuration file does not exists so we return an empty array
+            return [];
+        }
+
+        const {
+            compilerOptions,
+            files,
+            include: includes,
+            exclude: excludes,
+        } = JSON.parse(this.fileSystem.readFile(typescriptConfigurationFile));
+
+        // Check if the "rootDir" entry is defined
+        if (typeof compilerOptions.rootDir === 'string') {
+            this.logger.silly(
+                `Adding the rootdir to the found results: ${compilerOptions.rootDir}`,
+            );
+            result.push(this.fileSystem.resolve(path, compilerOptions.rootDir));
+        }
+
+        // Check if the "rootDir" entry is defined
+        if (Array.isArray(compilerOptions.rootDirs)) {
+            this.logger.silly(
+                `Adding the rootdirs to the found results: ${compilerOptions.rootDirs.join(
+                    ', ',
+                )}`,
+            );
+            result.push(
+                ...compilerOptions.rootDirs.map((directory: string) =>
+                    this.fileSystem.resolve(path, directory),
+                ),
+            );
+        }
+
+        // Check if the "files" entry is defined which should be added to the result
+        if (Array.isArray(files)) {
+            this.logger.debug(
+                `Adding the following files to the result: ${files.join(', ')}`,
+            );
+            result.push(
+                ...files.reduce<string[]>(
+                    (acc, entry) => acc.concat(this.resolveGlob(entry, path)),
+                    [],
+                ),
+            );
+        }
+
+        // Check if there are any includes which should be added to the results
+        if (Array.isArray(includes)) {
+            this.logger.debug(
+                `Adding the following includes to the result: ${includes.join(
+                    ', ',
+                )}`,
+            );
+            result.push(
+                ...includes.reduce<string[]>(
+                    (acc, entry) => acc.concat(this.resolveGlob(entry, path)),
+                    [],
+                ),
+            );
+        }
+
+        // Check if there are any excludes defined which should be removed from the results
+        if (Array.isArray(excludes)) {
+            this.logger.debug(
+                `Removing the following excludes from the result: ${excludes.join(
+                    ', ',
+                )}`,
+            );
+
+            result = result.filter(
+                (entry) =>
+                    excludes.find((excludeEntry) =>
+                        entry.includes(excludeEntry),
+                    ) === undefined,
+            );
+        }
+
+        return result;
+    }
+
+    /**
+     * Resolves the given as "glob" to absolut file paths
+     *
+     * @private
+     * @param {string} glob The glob which should be resolved
+     * @param {string} path The path which should be used to resolve the entries
+     * @return {string[]} The resolved glob contents
+     * @memberof NPMPackageResolver
+     */
+    private resolveGlob(glob: string, path: string): string[] {
+        if (!hasMagic(glob)) {
+            return [this.fileSystem.resolve(path, glob)];
+        }
+
+        return sync(glob).map((entry) => this.fileSystem.resolve(path, entry));
     }
 }
